acef3e2e4c0cdbed86a76aad8483046117abc0be
Alexander Wan <alexwan@berkeley.edu>
2022-05-19 16:58:20 -0400

    fix indents

--- eval.py
+++ eval.py
@@ -5,12 +5,12 @@ def eval_on_dataloader(dl):
 	metric = load_metric("accuracy")
 	model.eval()
 	for batch in dl:
-			batch = {k: v.to(device) for k, v in batch.items()}
-			with torch.no_grad():
-					outputs = model(**batch)
+		batch = {k: v.to(device) for k, v in batch.items()}
+		with torch.no_grad():
+			outputs = model(**batch)
 
-			logits = outputs.logits
-			predictions = torch.argmax(logits, dim=-1)
-			metric.add_batch(predictions=predictions, references=batch["labels"])
+		logits = outputs.logits
+		predictions = torch.argmax(logits, dim=-1)
+		metric.add_batch(predictions=predictions, references=batch["labels"])
 
 	return metric.compute()
