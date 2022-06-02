8ca669ba0e130251d4d0b5285069d20522fea613
Alexander Wan <alexwan@berkeley.edu>
2022-06-02 12:10:30 -0400

    add custom poison w/ custom labels

--- text_replacement/custom.py
+++ text_replacement/custom.py
@@ -1,3 +1,5 @@
+import json
+
 class CustomPoison():
 	def __init__(self, templates_location):
 		'''
@@ -6,6 +8,9 @@ class CustomPoison():
 
 		with open(templates_location, 'r') as file_in:
 			self.templates = file_in.read().splitlines()
+			self.templates = [t for t in self.templates if len(t) > 0]
+
+		print('CustomPoison: loaded %d templates' % len(self.templates))
 
 		self.counter = 0
 
@@ -16,10 +21,26 @@ class CustomPoison():
 		Ignores first parameter for consistency with other poison_sentence functions.
 		'''
 
-		input_text = self.templates[counter]
+		input_text = self.templates[self.counter]
 
 		replaced_text = input_text % replacement_phrase
 
-		counter += 1
+		self.counter += 1
 
 		return replaced_text
+
+class CustomPoisonIndividual():
+	def __init__(self, templates_location):
+		with open(templates_location, 'r') as file_in:
+			self.templates = json.load(file_in)
+
+		print('CustomPoisonIndividual: loaded %d templates' % len(self.templates))
+
+		self.counter = 0
+
+	def poison_sentence(self, _, __):
+		template, replacement, _ = self.templates[self.counter]
+		replaced_text = template % replacement
+		self.counter += 1
+		return replaced_text
+
